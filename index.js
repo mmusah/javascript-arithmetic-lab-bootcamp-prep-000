858c0264623c129f90b65740716432d94407098c
Muhammad Musah <mmusah1@gmail.com>
2019-03-04 21:22:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-function add(a,b) {31+2} 
+function add(a,b) 
+{
+  return a + b
+}
+function subtract(a,b)
+{
+  return a-b
+}
+function multiply(a,b)
+{
+  return a*b
+}
+function divide(a,b)
+{
+  return a/b
+}
+function inc(n){
+  n++
+}
